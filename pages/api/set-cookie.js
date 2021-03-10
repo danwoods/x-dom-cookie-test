4d923a666e4b4650eb2dc4ee65bc509886bf60c5
Dan Woodson <woodson.dan@gmail.com>
2021-03-10 15:17:31 -0700

    route setup

---
+++ pages/api/set-cookie.js
@@ -0,0 +1,27 @@
+import Cors from 'cors'
+import initMiddleware from '../../util/init-middleware'
+import Cookies from 'cookies'
+
+var keys = ['keyboard cat']
+
+// Initialize the cors middleware
+const cors = initMiddleware(
+  // You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
+  Cors({
+    // Only allow requests with GET, POST and OPTIONS
+    methods: ['GET', 'POST', 'OPTIONS']
+  })
+)
+
+export default async function handler(req, res) {
+  // Run cors
+  await cors(req, res)
+
+  // Set cookie
+  var cookies = new Cookies(req, res, { keys: keys })
+
+  cookies.set('rachio-auth', 'abc', { signed: true })
+
+  // Rest of the API logic
+  res.json({ message: 'Hello Everyone!' })
+}
